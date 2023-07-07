3facb8efeded9ee943d2ea6a8dbe9da849ac78c7
andreyaici3 <andreyandri90@gmail.com>
2023-07-07 13:20:14 +0700

    review 1 - modal filter pages

--- lib/ui_features/pages/modal/filter_modal_pages.dart
+++ lib/ui_features/pages/modal/filter_modal_pages.dart
@@ -8,7 +8,7 @@ class FilterModalPages extends StatelessWidget {
   FilterModalPages({super.key});
   static const String filterModalPages = "filterModalPages";
 
-  List<SortModel> sortData = [
+  final List<SortModel> sortData = [
     SortModel(name: "All Categories", status: false),
     SortModel(name: "Smart Watches", status: true),
     SortModel(name: "Cell Phones & Accessories", status: true),
@@ -37,12 +37,12 @@ class FilterModalPages extends StatelessWidget {
 }
 
 class ModalFilterBody extends StatelessWidget {
-  ModalFilterBody({
+  const ModalFilterBody({
     super.key,
     required this.sm,
   });
 
-  List<SortModel> sm;
+  final List<SortModel> sm;
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +51,7 @@ class ModalFilterBody extends StatelessWidget {
         vertical: 40,
         horizontal: 20,
       ),
-      height: screenHeight(context) * 0.41,
+      height: screenHeight(context) * 0.5,
       decoration: const BoxDecoration(),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -61,7 +61,7 @@ class ModalFilterBody extends StatelessWidget {
             style: AssetStyles.h2,
           ),
           verticalSpace(20),
-          Container(
+          SizedBox(
             height: screenHeight(context) * 0.25,
             child: ListView.builder(
               itemCount: sm.length,
@@ -77,9 +77,7 @@ class ModalFilterBody extends StatelessWidget {
                     children: [
                       Text(
                         sm[index].name,
-                        style: AssetStyles.labelMdMdReg.copyWith(
-                          color: AssetColors.inkDarkest,
-                        ),
+                        style: AssetStyles.labelMdRegular,
                       ),
                       Checkbox(value: sm[index].status, onChanged: (vale) {}),
                     ],
@@ -92,15 +90,15 @@ class ModalFilterBody extends StatelessWidget {
           ButtonPrimary(
             onTap: () {},
             text: "Show 340 Result",
-            height: 50,
+            height: 40,
+            width: screenWidth(context) * 0.9,
           ),
           verticalSpace(20),
           Center(
             child: TextButtonCustom(
               text: "Reset",
-              style: AssetStyles.labelMdMdReg.copyWith(
-                color: AssetColors.inkDarkest,
-              ),
+              style: AssetStyles.labelMdRegular
+                  .copyWith(color: AssetColors.inkDarkest),
               onTap: () {},
             ),
           ),
