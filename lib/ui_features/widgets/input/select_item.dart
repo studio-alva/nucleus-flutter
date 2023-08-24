c201890b40340f289e1ec88ede6fcec4f9a55687
farishasyim <faris.hasyim.03@gmail.com>
2023-08-24 12:12:55 +0700

    enhance select item widget

--- lib/ui_features/widgets/input/select_item.dart
+++ lib/ui_features/widgets/input/select_item.dart
@@ -21,7 +21,7 @@ class SelectItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    return GestureDetector(
       onTap: onTap,
       child: Container(
         margin: const EdgeInsets.only(bottom: 15),
