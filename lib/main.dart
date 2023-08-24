14a1832218f3060347bca98db9f9d1ca0e2f5394
alamsyahh15 <black.taurus001@gmail.com>
2023-08-24 13:33:45 +0700

    update for website preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
@@ -6,7 +7,7 @@ import 'app/app.dart';
 void main(List<String> args) {
   runApp(
     DevicePreview(
-      enabled: true,
+      enabled: kReleaseMode,
       builder: (BuildContext context) => const App(),
     ),
   );
